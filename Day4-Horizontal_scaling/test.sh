849c82a806760cd4f33f762f846776ac9ae7577d
Kedarling7838 <kedarlingkanade@gmail.com>
2026-07-27 09:40:04 +0530

    Modify test.sh to use curl for load testing
    Replace CPU load generation with concurrent curl requests.

--- Day4-Horizontal_scaling/test.sh
+++ Day4-Horizontal_scaling/test.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
+for j in {1..10}; do
+    for i in {1..1000}; do
+        curl -s -o /dev/null -w "%{http_code}\n" http://a48d1d51f2945469da6ffc19238aa8ad-1307616002.ap-south-1.elb.amazonaws.com/ &
+    done
 
-echo "Generating CPU load on nginx..."
-
-kubectl run -i --tty load-generator \
-  --rm \
-  --image=busybox \
-  --restart=Never \
-  -- /bin/sh -c "while true; do wget -q -O- http://nginx-service; done"
+    # Wait for all background curl processes to finish before next iteration
+    wait
+done
